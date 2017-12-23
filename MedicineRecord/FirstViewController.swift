bcaa54c5b6b8c52dc12847aec8369d5d1df7b6e9
Rajeev Rajeshuni <Rajeev@apples-MacBook-Pro.local>
2017-12-23 12:05:46 +0530

    Initial Commit

---
+++ MedicineRecord/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MedicineRecord
+//
+//  Created by Rajeev Rajeshuni on 12/23/17.
+//  Copyright © 2017 Rajeev Rajeshuni. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MedicineRecord/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MedicineRecord
+//
+//  Created by Rajeev Rajeshuni on 12/23/17.
+//  Copyright © 2017 Rajeev Rajeshuni. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
